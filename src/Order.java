4857d8d102f7d31c6963f96a283f81b0bfa964a8
Quochuynh <letrinhquoch@gmail.com>
2022-08-09 23:19:14 +0700

    Add total_price instance to Order class

--- src/Order.java
+++ src/Order.java
@@ -7,6 +7,7 @@ public class Order implements Serializable {
     private Order_Item[] items;
     private String status;
     private Date date;
+    private double total_price;
     private static int count = 0;
 
     public Order(Member customer, Order_Item[] items, String status, Date date) {
@@ -15,6 +16,15 @@ public class Order implements Serializable {
         this.status = status;
         this.date = date;
         this.id = "O" + ++count;
+        this.total_price = 0;
+        for (Order_Item item : this.items){
+            this.total_price = this.total_price + item.getProduct().getPrice()*item.getQuantity();
+        }
+        switch (this.customer.getMembership()){
+            case "Silver": this.total_price = this.total_price * 0.95;
+            case "Gold": this.total_price = this.total_price * 0.9;
+            case "Platinum": this.total_price = this.total_price * 0.85;
+        }
     }
 
     public String getId() {
@@ -51,4 +61,6 @@ public class Order implements Serializable {
     public void setDate(Date date) {
         this.date = date;
     }
+
+    public double getTotal_price() { return total_price; }
 }
