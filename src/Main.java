2ef67974316a8a223664843eb7427577d0bd05ec
Dau Hoang Minh <s3926201@rmit.edu.vn>
2022-08-09 16:23:10 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
         // Print welcome screen
         System.out.println("COSC2081 GROUP ASSIGNMENT");
         System.out.println("STORE ORDER MANAGEMENT SYSTEM ");
@@ -10,6 +12,24 @@ public class Main {
         System.out.println("s3926201, Dau Hoang Minh ");
         System.out.println("s3927234, Dang Thai Hoang ");
         System.out.println("s3924345, Doan Huy ");
+        System.out.println("--------------------------------------------------------------");
+
+        // A loop that let users do multiple actions before they quit
+        while (true) {
+            System.out.println("Please enter a number correspond to any action as shown below!");
+            System.out.println("1 - Register/ Login to existing account"); // Then choose to register or login
+            System.out.println("2 - List all available products"); // Then customer can see a product detail
+            System.out.println("3 - Search products by a category");
+            System.out.println("4 - Sort all products by price");
+            System.out.println("5 - Exit");
+//            int choice = s.nextInt(); // Need to validate input
+//            switch (choice) {
+//                case 1:
+//            }
+            break;
+
+        }
+
 
     }
 }
