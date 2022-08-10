c73e05d70181c7a1a88baffcc369cf955de495d4
Quochuynh <letrinhquoch@gmail.com>
2022-08-11 01:15:57 +0700

    add readOrders()

--- src/ListOfOrder.java
+++ src/ListOfOrder.java
@@ -1,5 +1,24 @@
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
+import java.io.File;
 
 public class ListOfOrder {
-
+    private ArrayList<Order> orderList;
+    public ArrayList<Order> getOrderList() {
+        return orderList;
+    }
+    public void setOrderList(ArrayList<Order> orderList) {
+        this.orderList = orderList;
+    }
+    public void readOrders() throws Exception{
+        File ordersPackage = new File("Orders");
+        File[] orders = ordersPackage.listFiles();
+        for (File f : orders){
+            FileInputStream fi = new FileInputStream(f.getAbsolutePath());
+            ObjectInputStream orderIn = new ObjectInputStream(fi);
+            Order order = (Order) orderIn.readObject();
+            this.orderList.add(order);
+        }
+    }
 }
