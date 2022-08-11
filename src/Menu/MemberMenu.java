3d5f042e9d87024c81e096bac73193d9223570f7
Your Namethaihoangdang <thaihoangdang1803@gmail.com>
2022-08-11 17:33:26 +0700

    add Member Menu

--- src/Menu/MemberMenu.java
+++ src/Menu/MemberMenu.java
@@ -2,16 +2,26 @@ package Menu;
 
 import Account.Member;
 import Product.ListOfProduct;
+import Order.Order;
+import Order.ListOfOrder;
 
 import java.util.Scanner;
 
 public class MemberMenu extends GuestMenu {
     public static void printMenu() {
-
+        System.out.println("1 - List all available products"); // Then customer can see a product detail
+        System.out.println("2 - Search products by a category");
+        System.out.println("3 - Sort all products by price");
+        System.out.println("4 - Create an order");
+        System.out.println("5 - Search order by ID");
+        System.out.println("6 - Exit");
     }
+
+
     public static void main(Member member) {
         Scanner sc = new Scanner(System.in);
         ListOfProduct productList = new ListOfProduct();
+        ListOfOrder orderList = new ListOfOrder();
         productList.readProducts();
         while (true) {
             try {
@@ -20,15 +30,17 @@ public class MemberMenu extends GuestMenu {
                 System.out.print("Please enter a number correspond to any action as shown below!\n");
                 int n = sc.nextInt();
                 if (n == 1) {
-
+                    productList.viewAllProduct();
                 } else if (n == 2) {
-
-                } else if (n == 3) {
                     searchProductByCategory(productList);
+                } else if (n == 3) {
+                    productList.sortProductByPrice();
                 } else if (n == 4) {
-
+                    Order.create_order(member);
                 } else if (n == 5) {
-
+                    System.out.println("Enter order ID: ");
+                    String ID = sc.nextLine();
+                    orderList.searchOrder(ID);
                 } else if (n == 6) {
                     break;
                 } else {
